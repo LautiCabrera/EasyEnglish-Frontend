f602c1c459eadd2bed6a19e64965870b9385810e
AxelGurgone <gurgone.ramon@yahoo.com>
2024-10-15 20:03:33 -0300

    replanteo de cards y modals. falta terminar

--- script/logica/load-cards.js
+++ script/logica/load-cards.js
@@ -1,14 +1,14 @@
-function openModal(imageSrc, infoText) {
-  const modal = document.getElementById('modal');
-  const modalImage = document.getElementById('modal-image');
-  const modalInfo = document.getElementById('modal-info');
+// function openModal(imageSrc, infoText) {
+//   const modal = document.getElementById('modal');
+//   const modalImage = document.getElementById('modal-image');
+//   const modalInfo = document.getElementById('modal-info');
 
-  modalImage.src = imageSrc;
-  modalInfo.textContent = infoText;
-  modal.style.display = 'block';
-}
+//   modalImage.src = imageSrc;
+//   modalInfo.textContent = infoText;
+//   modal.style.display = 'block';
+// }
 
-function closeModal() {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'none';
-}
+// function closeModal() {
+//   const modal = document.getElementById('modal');
+//   modal.style.display = 'none';
+// }
