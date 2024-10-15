c8ade54384c40622604e06a61c78dd46282019a1
AxelGurgone <gurgone.ramon@yahoo.com>
2024-10-15 15:19:19 -0300

    intento de corparacion de modal

---
+++ script/logica/load-cards.js
@@ -0,0 +1,14 @@
+function openModal(imageSrc, infoText) {
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modal-image');
+  const modalInfo = document.getElementById('modal-info');
+
+  modalImage.src = imageSrc;
+  modalInfo.textContent = infoText;
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
