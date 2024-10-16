58566c0cce685d4e98fea9d8872bbc3414786d8c
AxelGurgone <gurgone.ramon@yahoo.com>
2024-10-15 23:40:37 -0300

    primer modal funcional cargado

--- script/logica/load-cards.js
+++ script/logica/load-cards.js
@@ -1,14 +1,10 @@
-// function openModal(imageSrc, infoText) {
-//   const modal = document.getElementById('modal');
-//   const modalImage = document.getElementById('modal-image');
-//   const modalInfo = document.getElementById('modal-info');
+const open = document.getElementById('open');
+const modal_con = document.getElementById('modal-con');
+const close = document.getElementById('close');
 
-//   modalImage.src = imageSrc;
-//   modalInfo.textContent = infoText;
-//   modal.style.display = 'block';
-// }
-
-// function closeModal() {
-//   const modal = document.getElementById('modal');
-//   modal.style.display = 'none';
-// }
+open.addEventListener('click', () => {
+    modal_con.classList.add('show');
+});
+close.addEventListener('click', () => {
+    modal_con.classList.remove('show');
+});
