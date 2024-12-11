98cc886fc6e80a226031c95c431f42992363350f
Lautaro Cabrera <lau.cabrera114@gmail.com>
2024-12-11 15:07:24 -0300

    Cambio condicional en navbarLoaded

--- script/navbar.js
+++ script/navbar.js
@@ -7,33 +7,23 @@ const navbarLoaded = () => {
     // Cambiar botón a "Cerrar sesión"
     loginButton.textContent = "Cerrar Sesión";
     loginButton.onclick = logout;
-
-    // Obtener el rol del usuario
-    const role = getUserRole();
-
-    // Filtrar rutas según el rol
-    links.forEach((link) => {
-      const href = link.getAttribute("href");
-      const parentLi = link.closest("li");
-
-      // Ocultar "profesores.html" solo para estudiantes
-      if (role != "teacher" && href.includes("profesores")) {
-        if (parentLi) parentLi.style.display = "none";
-      }
-    });
   } else {
     // Usuario no autenticado
     loginButton.textContent = "Conectarse";
     loginButton.onclick = () => (location.href = "../pages/login.html");
-
-    // Ocultar solo "profesores.html"
-    links.forEach((link) => {
-      const href = link.getAttribute("href");
-      const parentLi = link.closest("li");
-
-      if (href.includes("profesores.html") && parentLi) {
-        parentLi.style.display = "none";
-      }
-    });
   }
+
+  // Obtener el rol del usuario
+  const role = getUserRole();
+
+  // Filtrar rutas según el rol
+  links.forEach((link) => {
+    const href = link.getAttribute("href");
+    const parentLi = link.closest("li");
+
+    // Ocultar "profesores.html" solo para estudiantes
+    if (role != "teacher" && href.includes("profesores")) {
+      if (parentLi) parentLi.style.display = "none";
+    }
+  });
 };
